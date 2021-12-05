33b8f1aae8719dcb5a6ca559b093c7550e3448f8
Paf <pavlinpenevv@abv.bg>
2021-12-05 18:29:42 +0200

    Update AggregateElements.js

--- Syntax Functions And Statements - Lab/AggregateElements.js
+++ Syntax Functions And Statements - Lab/AggregateElements.js
@@ -1,4 +1,4 @@
-function aggregate(array = []){
+function aggregate(array){
     let sum = 0;
     let sumInverse = 0;
     let concat = "";
@@ -6,7 +6,7 @@ function aggregate(array = []){
     sum = array.reduce((partSum, a) => partSum + a, 0);
     sumInverse = array.reduce((partSum, a) => partSum + 1/a, 0);
     concat = array.join("");
-    
+
     console.log(sum);
     console.log(sumInverse);
     console.log(concat);
