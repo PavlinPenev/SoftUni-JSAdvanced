ae1e861b126edcfcf5e6405e3685c744ad28a993
Paf <pavlinpenevv@abv.bg>
2021-12-07 22:29:58 +0200

    Arrays And Nested Arrays - Exercise

--- Arrays And Nested Arrays - Exercise/ListOfNames.js
+++ Arrays And Nested Arrays - Exercise/ListOfNames.js
@@ -1,5 +1,5 @@
 function listOfNames(array = []) {
-    array.sort();
+    array.sort((a, b) => a.localeCompare(b));
     for(let i = 0; i < array.length; i++) {
         console.log(`${i + 1}.${array[i]}`);
     }
