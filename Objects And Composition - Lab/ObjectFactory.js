b95fb48219f53c7572acd02d1330a68a70bde075
Paf <pavlinpenevv@abv.bg>
2021-12-12 18:52:47 +0200

    Objects And Compositions - Lab

---
+++ Objects And Composition - Lab/ObjectFactory.js
@@ -0,0 +1,42 @@
+function factory(library, orders = []) {
+
+    return orders.map(composeObject);
+    function composeObject(order) {
+    let finalOrder = Object.assign({}, order.template);
+    for(const part of order.parts) {
+        finalOrder[part] = library[part];
+    }
+    return finalOrder;
+    }
+}
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  
+const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']      
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']      
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']      
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']      
+    }
+  ];
+console.log(factory(library, orders));
