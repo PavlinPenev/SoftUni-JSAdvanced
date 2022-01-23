befaa6617ea9d5d3c226acfb1f8476e09b86626f
Paf <pavlinpenevv@abv.bg>
2022-01-23 17:09:12 +0200

    Unit Testing And Error Handling - Lab

--- Unit Testing And Error Handling - Lab/test/test.js
+++ Unit Testing And Error Handling - Lab/test/test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const sum = require('./sumOfNumbers');
 const isSymmetric = require('./checkSymmetry');
 const rgbToHexColor = require('./rgbToHex');
+const createCalculator = require('./createCalculator');
 
 describe('Sum Of Numbers Tests', () => {
 
@@ -93,6 +94,70 @@ describe('RGB to HEX Tests', () => {
         expect(rgbToHexColor(255, 0, 0)).to.equal('#FF0000');
         expect(rgbToHexColor(0, 255, 0)).to.equal('#00FF00');
         expect(rgbToHexColor(0, 0, 255)).to.equal('#0000FF');
+
+    })
+})
+
+describe('Add Subtract Tests', () => {
+
+    it('returns an object as a result of the function', () => {
+
+        expect(typeof createCalculator()).to.equal('object');
+
+    })
+
+    it('contains the three functions add, subtract, get as properties', () => {
+
+        const object = createCalculator();
+        expect(object).haveOwnProperty('add');
+        expect(object).haveOwnProperty('subtract');
+        expect(object).haveOwnProperty('get');
+
+    })
+
+    it('gets properly', () => {
+
+        const object = createCalculator();
+        object.add(3);
+        expect(object.get()).to.equal(3);
+
+    })
+
+    it('sums properly', () => {
+
+        const object = createCalculator();
+        object.add(3);
+        object.add(3);
+        expect(object.get()).to.equal(6);
+        object.add(5);
+        expect(object.get()).to.equal(11);
+
+    })
+
+    it('subtracts properly', () => {
+
+        const object = createCalculator();
+        object.add(3);
+        object.subtract(2);
+        expect(object.get()).to.equal(1);
+        object.subtract(10);
+        expect(object.get()).to.equal(-9);
+
+    })
+
+    it('calculates properly if a string represantation of a number is given', () => {
+
+        const object = createCalculator();
+        object.add('1');
+        expect(object.get()).to.equal(1);
+
+    })
+
+    it('returns NaN if not a number or not a string represantation of a number is given', () => {
+
+        const object = createCalculator();
+        object.add('blah');
+        expect(Number.isNaN(object.get())).to.be.true;
 
     })
 })
