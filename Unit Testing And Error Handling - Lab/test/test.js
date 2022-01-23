c3ff58ca643c1558f7b569dbd4d705f3cc4300e7
Paf <pavlinpenevv@abv.bg>
2022-01-23 15:44:00 +0200

    Unit Testing And Error Handling - Lab

--- Unit Testing And Error Handling - Lab/test/test.js
+++ Unit Testing And Error Handling - Lab/test/test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const sum = require('./sumOfNumbers');
 const isSymmetric = require('./checkSymmetry');
+const rgbToHexColor = require('./rgbToHex');
 
 describe('Sum Of Numbers Tests', () => {
 
@@ -59,4 +60,39 @@ describe('Check for Symmetry Tests', () => {
             expect(isSymmetric([1,2,2,'1'])).to.be.false;
 
         })
+})
+
+describe('RGB to HEX Tests', () => {
+
+    it('returns undefined if out of range parameters', () => {
+
+        expect(rgbToHexColor(-3,5,196)).to.equal(undefined);
+        expect(rgbToHexColor(3,-5,196)).to.equal(undefined);
+        expect(rgbToHexColor(3,5,-196)).to.equal(undefined);
+
+        expect(rgbToHexColor(3,5,256)).to.equal(undefined);
+        expect(rgbToHexColor(3,256,6)).to.equal(undefined);
+        expect(rgbToHexColor(300,5,56)).to.equal(undefined);
+
+    })
+
+    it('returns undefined if parameters are not numbers', () => {
+
+        expect(rgbToHexColor('3',5,196)).to.equal(undefined);
+        expect(rgbToHexColor(3,'5',196)).to.equal(undefined);
+        expect(rgbToHexColor(3,5,'196')).to.equal(undefined);
+        
+    })
+
+    it('returns proper hex represantations', () => {
+
+        expect(rgbToHexColor(66, 135, 245)).to.equal('#4287F5');
+        expect(rgbToHexColor(5, 10, 18)).to.equal('#050A12');
+        expect(rgbToHexColor(79, 9, 23)).to.equal('#4F0917');
+
+        expect(rgbToHexColor(255, 0, 0)).to.equal('#FF0000');
+        expect(rgbToHexColor(0, 255, 0)).to.equal('#00FF00');
+        expect(rgbToHexColor(0, 0, 255)).to.equal('#0000FF');
+
+    })
 })
