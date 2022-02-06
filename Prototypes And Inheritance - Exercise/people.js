ca7a6fe99a6a7bf4ca69d963b817a9747481fd58
Paf <pavlinpenevv@abv.bg>
2022-02-06 19:45:26 +0200

    Prototypes And Inheritance - Exercise

--- Prototypes And Inheritance - Exercise/people.js
+++ Prototypes And Inheritance - Exercise/people.js
@@ -79,28 +79,3 @@ function people() {
     }
 
 }
-const classes = people(); 
-const junior = new classes.Junior('Ivan',25); 
- 
-junior.work();  
-junior.work();  
- 
-junior.salary = 5811; 
-junior.collectSalary();  
- 
-const sinior = new classes.Senior('Alex', 31); 
- 
-sinior.work();  
-sinior.work();  
-sinior.work();  
-sinior.work();  
- 
-sinior.salary = 12050; 
-sinior.collectSalary();  
- 
-const manager = new classes.Manager('Tom', 55); 
- 
-manager.salary = 15000; 
-manager.collectSalary();  
-manager.dividend = 2500; 
-manager.collectSalary();  
