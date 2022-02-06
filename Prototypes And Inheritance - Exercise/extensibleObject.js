7538fe5b70e6a2972064e79f46985b473e0538ec
Paf <pavlinpenevv@abv.bg>
2022-02-06 18:33:37 +0200

    Prototypes And Inheritance - Exercise

--- Prototypes And Inheritance - Exercise/extensibleObject.js
+++ Prototypes And Inheritance - Exercise/extensibleObject.js
@@ -23,13 +23,3 @@ function extensibleObject() {
     return this;
 
 }
-
-const myObj = extensibleObject();
-var template = {
-    extensionMethod: function () {
-        console.log("From extension method")
-    }
-};
-
-myObj.extend(template);
-console.log(Object.getPrototypeOf(myObj).hasOwnProperty('extensionMethod'));
