b6914c0391681738254d70db8daa106bfaf995d7
Paf <pavlinpenevv@abv.bg>
2022-02-06 19:10:51 +0200

    Prototypes And Inheritance - Exercise

--- Prototypes And Inheritance - Exercise/balloons.js
+++ Prototypes And Inheritance - Exercise/balloons.js
@@ -71,11 +71,3 @@ function balloons() {
     }
 
 }
-
-let classes = balloons();
-let testBalloon = new classes.Balloon("yellow", 20.5);
-let testPartyBalloon = new classes.PartyBalloon("yellow", 20.5, "red", 10.25);
-let ribbon = testPartyBalloon.ribbon;
-console.log(testBalloon);
-console.log(testPartyBalloon);
-console.log(ribbon);
